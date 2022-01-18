cc587edbb19a3a0580f9078d7a51fc4129bbefab
Qiming-Liu <prossliu@gmail.com>
2022-01-19 03:34:54 +1030

    Add axios output

--- src/combination/WeatherCard/index.js
+++ src/combination/WeatherCard/index.js
@@ -1,34 +1,30 @@
 import './index.css';
 
 import axios from 'axios';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 const WeatherCard = () => {
   const [getting, setGetting] = useState(false);
+  const [city, setCity] = useState('London');
+  const [lastCity, setLastCity] = useState('');
   const [weatherData, setWeatherData] = useState([]);
 
-  useEffect(() => {
-    // if (getting) return;
-    // setGetting(true);
-    axios
-      .get(
-        'http://api.weatherapi.com/v1/current.json?key=10ae5df6e5644e3c9d882939221701&q=London&aqi=no'
-      )
-      .then(res => {
-        setWeatherData(res.data);
-        console.log(res.data);
-        // setGetting(false);
-      })
-      .catch(error => {
-        // setGetting(false);
-      });
-  });
+  const getWeather = async city => {
+    if (city === lastCity) return;
+    if (getting) return;
+    setGetting(true);
+    const { data } = await axios.get(
+      `http://api.weatherapi.com/v1/current.json?key=10ae5df6e5644e3c9d882939221701&q=${city}&aqi=no`
+    );
+    setGetting(false);
+    setLastCity(city);
+    setWeatherData(data);
+    console.log(data);
+  };
 
-  return (
-    <ul>
-      {JSON.stringify(weatherData)}
-    </ul>
-  );
+  getWeather(city);
+
+  return <ul>{JSON.stringify(weatherData)}</ul>;
 };
 
 export default WeatherCard;
